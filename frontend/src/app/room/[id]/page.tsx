08c00834e532053055525341dc83f03db986dfff
Manuel dos Santos <manuelds@outlook.pt>
2024-04-05 18:47:49 +0100

    feat: :lipstick: mirro-mode

--- frontend/src/app/room/[id]/page.tsx
+++ frontend/src/app/room/[id]/page.tsx
@@ -43,7 +43,7 @@ export default function Room({ params }: { params: { id: string } }) {
                 <div className="md:w-[85%] w-full m-3">
                     <div className="grid md:grid-cols-2 grid-cols-1 gap-8">
                         <div className="bg-gray-950 w-full rounded-md h-full relative">
-                            <video src="" className="w-full h-full " ref={localStream}>
+                            <video src="" className="w-full h-full " autoPlay ref={localStream}>
                             </video>
                             <span className="absolute bottom-3" >VIDEO_user_name</span>
                         </div>
