40f8f4b0874408d41ca550609834edbaa6e46ca3
Manuel dos Santos <manuelds@outlook.pt>
2024-04-07 18:59:30 +0100

    feat: :sparkles: add peerConnection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -33,23 +33,32 @@ class App {
         socket.on('subscribe', (data) => {
             console.log('usuário inserido na sala: ' + data.roomId)
             socket.join(data.roomId)
-
+            socket.join(data.socketId)
+            //array de rooms| prop rooms é nativa do socket
             const roomSession = Array.from(socket.rooms)
             if (roomSession.length > 1) {
                 socket.to(data.roomId).emit('new user', {
-                    socketId: socket.id
-                    username
+                    socketId: socket.id,
+                    username: data.username
                 })
             }
-            socket.on('chat', (data) => {
-                console.log("Socket: ", data)
-                socket.broadcast.to(data.roomId).emit('chat', {
-                    message: data.message,
-                    username: data.username,
-                    time: data.time
-                })
-            })
 
+        })
+        socket.on('newUserStart', (data) => {
+            console.log('Entrou um novo usuário: ', data)
+            socket.to(data.to).emit('newUserStart', {
+                sender: data.sender
+            })
+        })
+
+
+        socket.on('chat', (data) => {
+            console.log("Socket: ", data)
+            socket.broadcast.to(data.roomId).emit('chat', {
+                message: data.message,
+                username: data.username,
+                time: data.time
+            })
         })
     }
 }
--- frontend/src/app/room/[id]/page.tsx
+++ frontend/src/app/room/[id]/page.tsx
@@ -9,6 +9,7 @@ import { SocketContext } from "../../../contexts/SocketContext";
 export default function Room({ params }: { params: { id: string } }) {
     const { socket } = useContext(SocketContext)
     const localStream = useRef<HTMLVideoElement>(null)
+    const peerConnection = useRef<Record<string, RTCPeerConnection>>({})
     useEffect(() => {
         socket?.on('connect', async () => {
             console.log('conectado');
@@ -18,8 +19,30 @@ export default function Room({ params }: { params: { id: string } }) {
             });
             await initCamera()
         });
+        console.log('kkey!')
+        socket?.on('new user', (data) => {
+            console.log('Novo usuário conectado:', data)
+            createPeerConnection(data.socketId)
+            // console.log('peeer!')
+            socket.emit('newUserStart', {
+                to: data.id,
+                sender: socket.id
+            })
+        })
     }, [socket]);
 
+    // PeerConnection!!
+    const createPeerConnection = (socketId: string) => {
+        const config = {
+            iceServer: [
+                {
+                    urls: 'stun:stun.l.google.com:9302'
+                }
+            ]
+        }
+        const peer = new RTCPeerConnection(config)
+        peerConnection.current[socketId] = peer
+    }
 
     const initCamera = async () => {
         const video = await navigator.mediaDevices.getUserMedia({
@@ -42,10 +65,10 @@ export default function Room({ params }: { params: { id: string } }) {
             <div className="flex h-[70%] ">
                 <div className="md:w-[85%] w-full m-3">
                     <div className="grid md:grid-cols-2 grid-cols-1 gap-8">
-                        <div className="bg-gray-950 w-full rounded-md h-full relative -scale-x-100">
-                            <video src="" className="w-full h-full " autoPlay ref={localStream}>
+                        <div className="bg-gray-950 w-full rounded-md h-full p-2 relative  ">
+                            <video src="" className="w-full h-full -scale-x-100 " autoPlay ref={localStream}>
                             </video>
-                            <span className="absolute bottom-3" >VIDEO_user_name</span>
+                            <span className="absolute bottom-3  mx-1" >VIDEO_user_name</span>
                         </div>
                         <div className="bg-gray-950 w-full rounded-md h-full relative">
                             <video src="" className="w-full h-full " autoPlay playsInline>
